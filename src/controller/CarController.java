1b13c102a63ebbe7971449ed224a28211f8b8d7f
Dzikri Muhammad <notfnd123@gmail.com>
2023-12-25 15:45:54 +0700

    initial commit

---
+++ src/controller/CarController.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author Dzikri
+ */
+public class CarController {
+    private int id;
+    private String plat;
+    private String merk;
+    private double harga;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the plat
+     */
+    public String getPlat() {
+        return plat;
+    }
+
+    /**
+     * @param plat the plat to set
+     */
+    public void setPlat(String plat) {
+        this.plat = plat;
+    }
+
+    /**
+     * @return the merk
+     */
+    public String getMerk() {
+        return merk;
+    }
+
+    /**
+     * @param merk the merk to set
+     */
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    /**
+     * @return the harga
+     */
+    public double getHarga() {
+        return harga;
+    }
+
+    /**
+     * @param harga the harga to set
+     */
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
+}
---
+++ src/controller/RentController.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author Dzikri
+ */
+public class RentController {
+    private String kode;
+    private String tanggal;
+    private String plat;
+    private double harga;
+    private int durasi;
+    private double total;
+
+    /**
+     * @return the kode
+     */
+    public String getKode() {
+        return kode;
+    }
+
+    /**
+     * @param kode the kode to set
+     */
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    /**
+     * @return the tanggal
+     */
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    /**
+     * @param tanggal the tanggal to set
+     */
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    /**
+     * @return the plat
+     */
+    public String getPlat() {
+        return plat;
+    }
+
+    /**
+     * @param plat the plat to set
+     */
+    public void setPlat(String plat) {
+        this.plat = plat;
+    }
+
+    /**
+     * @return the harga
+     */
+    public double getHarga() {
+        return harga;
+    }
+
+    /**
+     * @param harga the harga to set
+     */
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    /**
+     * @return the durasi
+     */
+    public int getDurasi() {
+        return durasi;
+    }
+
+    /**
+     * @param durasi the durasi to set
+     */
+    public void setDurasi(int durasi) {
+        this.durasi = durasi;
+    }
+
+    /**
+     * @return the total
+     */
+    public double getTotal() {
+        return total;
+    }
+
+    /**
+     * @param total the total to set
+     */
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    
+    
+}
